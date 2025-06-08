70de7b33f731d30361ffd4c950a95e39e44b5f9b
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-06-08 22:59:27 +0200

    fix: rename test_epoch_end to on_test_epoch_end for consistency

--- module.py
+++ module.py
@@ -162,7 +162,7 @@ class LaneModule(pl.LightningModule):
         losses = torch.mean(torch.stack([x for x in outputs]))
         self.log_dict({"val_loss_accumulated": losses }, batch_size=self.bs)
 
-    def test_epoch_end(self, outputs):
+    def on_test_epoch_end(self, outputs):
         losses = torch.mean(torch.stack([x for x in outputs]))
         self.log_dict({"test_loss_accumulated": losses }, batch_size=self.bs)
     
