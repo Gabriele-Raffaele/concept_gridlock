3c993b8bf538459964c9551bb1071e552efe5542
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-07-20 16:18:12 +0200

    fix: correct unpacking of predictions and remove unnecessary print statements for clarity

--- main.py
+++ main.py
@@ -177,10 +177,8 @@ if __name__ == "__main__":
             predictions, preds_1, preds_2 = pred[0], pred[1], pred[2] 
             save_preds(predictions, preds_1, f"{args.dataset}_{args.task}_{args.backbone}_{args.concept_features}_{args.n_scenarios}", save_path)
         else:
-            preds_tuple, attentions, probs = pred
-            logits_angle, logits_dist, _, _ = preds_tuple
+            (preds_tuple, attentions), probs = pred  # ✅ unpack corretto
+            logits_angle, logits_dist, _, _ = preds_tuple  # ✅ unpack corretto
             print(f"Logits angle shape: {logits_angle.shape}, Logits dist shape: {logits_dist.shape}")
-            print(type(logits_angle), type(logits_dist))
-            # Salva logits_angle e logits_dist (qui senza target)
             save_preds(logits_angle, logits_angle, f"angle_multi_{args.dataset}_{args.task}_{args.backbone}_{args.concept_features}", save_path)
             save_preds(logits_dist, logits_dist, f"dist_multi_{args.dataset}_{args.task}_{args.backbone}_{args.concept_features}", save_path)
