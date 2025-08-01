5c3f96c996380e810a832b9ec480bbc8b62a88e0
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-08-01 20:17:35 +0200

    fix: update dataset path in CommaDataset and clean up imports in intervention_explanation notebook

--- analyse_results/analyze_results.py
+++ analyse_results/analyze_results.py
@@ -32,7 +32,7 @@ nuscenes_ds = NUScenesDataset(dataset_type="test",
 
 commda_ds = CommaDataset(dataset_type="test",
         multitask=multitask,
-        ground_truth="normal", dataset_path='/data1/jessica/data/toyota/')
+        ground_truth="normal", dataset_path='"/kaggle/input/filtered-chunk-hdf5"')
 dataloader_comma = DataLoader(commda_ds, batch_size=1, shuffle=False, num_workers=0, collate_fn=pad_collate)
 
 model = VTN(multitask=multitask, backbone=backbone, concept_features=concept_features, device = f"cuda:{gpu_num}", return_concepts=True)
@@ -42,4 +42,6 @@ checkpoint_path = checkpoint_path_distance
 
 ckpt = torch.load(checkpoint_path)
 state_dict = ckpt['state_dict']
+#added this to load the model correctly -G.R.
+model.load_state_dict(state_dict)
 print('done')
--- dataloader_comma.py
+++ dataloader_comma.py
@@ -32,7 +32,6 @@ class CommaDataset(Dataset):
         self.return_full = return_full
         self.normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         self.resize = transforms.Resize((224,224))
-        #/data1/shared/jessica/data1/data/
         data_path = f"{dataset_path}/filtered_chunk1_{dataset_type}.hdf5" if ground_truth == "regular" else f"{dataset_path}/filtered_chunk1_{dataset_type}.hdf5"
         self.people_seqs = []
         self.h5_file = h5py.File(data_path, "r")
