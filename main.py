cec311b09f6736f2a114873df33826274cd43613
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-08-02 16:15:16 +0200

    fix: remove unused parameters from ModelCheckpoint in main function

--- main.py
+++ main.py
@@ -95,9 +95,6 @@ def main():
     #set where to save the checkpoints, when to save them with the ModelCheckpoint callback, and the logger for TensorBoard
     ckpt_pth = f"/kaggle/working/ckpts_final_{args.dataset}_{args.task}_{args.backbone}_{args.concept_features}_{args.dataset_fraction}"
     checkpoint_callback = ModelCheckpoint(save_top_k=2, 
-                                            dirpath=ckpt_pth,
-                                            filename='best-{epoch}-{val_loss:.2f}',
-                                           #mode="min", (peppe)
                                             monitor="val_loss_accumulated")
     
     logger = TensorBoardLogger(save_dir=ckpt_pth)
