3517bf5ae4d02ea4881b67ea0009efbab79977d7
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-08-21 01:30:44 +0200

    Add debug prints and reshape probabilities in VTN class

--- model.py
+++ model.py
@@ -160,8 +160,10 @@ class VTN(nn.Module):
             s = img.shape#[batch_size, seq_len, h,w,c]
             logits_per_image, logits_per_text = self.clip_model(img.reshape((img.shape[0]*img.shape[1], img.shape[2], img.shape[3], img.shape[4])), scenarios_tokens.to(x.device))
             #TODO: VERIFICARE SE QUA BISOGNA CAMBIARE SOFTMAX CON una di quelle segnate. (SOFTMAX REPLACE (2) )
-            probs = logits_per_image.softmax(dim=-1)
-            probs = probs.reshape((int(img.shape[0]), int(logits_per_image.shape[0]/img.shape[0]), -1))
+            print(f"DEBUG forward: logits_per_image={logits_per_image.shape}")
+            probs = logits_per_image.softmax(dim=-1) # [batch_size*seq_len, num_scenarios]
+            probs = probs.reshape((int(img.shape[0]), int(logits_per_image.shape[0]/img.shape[0]), -1)) #Reshape to [batch_size, seq_len, num_scenarios] -G.R.
+            print(f"DEBUG forward: probs={probs.shape}")
             if not self.train_concepts: probs = probs.detach()
 
         angle = torch.roll(angle, shifts=1, dims=1)
