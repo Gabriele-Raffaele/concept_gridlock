e858296459f2072f78cac105a0ff8a25485382da
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-08-28 00:10:41 +0200

    Add file processing to visualize_hdf5.py and update input file path

--- visualize_hdf5.py
+++ visualize_hdf5.py
@@ -1,9 +1,23 @@
 import h5py
 
-file_path = "/Users/gabriele/Desktop/Magistrale/Explainable_and_trustworthy_AI/progetti/concept_gridlock/filtered_chunk1_test.hdf5"
+file_path = "/Users/gabriele/Desktop/Magistrale/Explainable_and_trustworthy_AI/progetti/concept_gridlock/filtered_chunk1_train.hdf5"
 
 with h5py.File(file_path, "r") as f:
     print("Chiavi principali nel file HDF5:")
+    all_keys = list(f.keys())
+
+    with open("kept_seqs_modified.txt", "r") as txt_file:
+        kept_seqs = [line.strip() for line in txt_file.readlines()]
+
+    keys_in_txt = [key for key in all_keys if key in kept_seqs]
+    keys_not_in_txt = [key for key in all_keys if key not in kept_seqs]
+
+    print(f"Chiavi presenti sia nel file HDF5 che in kept_seqs.txt: {len(keys_in_txt)}")
+    print(f"Chiavi presenti nel file HDF5 ma mancanti in kept_seqs.txt: {len(keys_not_in_txt)}")
+    print("Chiavi mancanti in kept_seqs.txt:")
+    for key in keys_not_in_txt:
+        print(f" - {key}")
+    '''
     for key in f.keys():
         print(f" - {key}")
     
@@ -19,4 +33,4 @@ with h5py.File(file_path, "r") as f:
     
     print(f"\nContenuto del dataset '{first_dataset_name}':")
     print(data)
-    print(f"\nShape: {data.shape}, dtype: {data.dtype}")
+    print(f"\nShape: {data.shape}, dtype: {data.dtype}")'''
