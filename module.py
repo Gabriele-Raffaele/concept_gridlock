80ca1ef76461fe6d18d40fd035126a3ba7719afb
Peppe2212 <117827822+Peppe2212@users.noreply.github.com>
2025-08-30 17:39:09 +0200

    Add debug print statements for logits distance shape and loss calculation in calculate_loss method

--- module.py
+++ module.py
@@ -46,6 +46,7 @@ class LaneModule(pl.LightningModule):
             #ho messo logits[0] perchè il forward restituisce una tupla, prima era logits
             # (x[:,1:F+1,:], x2[:,1:F+1,:],self.multitask_param_angle, self.multitask_param_dist), attentions -G.R.
             logits_angle, logits_dist, param_angle, param_dist = res[0]
+            print("logits_distance shape before squeeze:", logits_dist.shape)
             mask = distance.squeeze() == 0.0
             #TODO: The intervention Boolean flag indicates whether 
             # a special type of supervision is being used in which:
@@ -74,6 +75,8 @@ class LaneModule(pl.LightningModule):
             mask = distance.squeeze() == 0.0
             logits = res[0] if isinstance(res, tuple) else res
             ##(3) Poi si arriva qui in singletask -- G.V.
+            print("logits_distance shape before squeeze:", logits.shape)
+
             loss = torch.sqrt(self.loss(logits.squeeze(), target.squeeze(), mask))
             print("loss distance in calculate loss:", loss)
             self.log_dict({"loss_inside_calcLoss_func": loss}, on_epoch=True, batch_size=self.bs, sync_dist=True)
