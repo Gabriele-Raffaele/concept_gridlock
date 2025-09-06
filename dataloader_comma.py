61755aca3802134d16aeaccbdea2356e47966e5c
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-09-06 17:35:23 +0200

    Refactor dataset path handling in CommaDataset class and clean up comments

--- dataloader_comma.py
+++ dataloader_comma.py
@@ -14,7 +14,6 @@ class CommaDataset(Dataset):
         multitask="angle",
         ground_truth="desired",
         return_full=False, 
-        dataset_path ="/kaggle/input/final-hdf5-files" ,
         dataset_fraction=1.0
     ):
         assert dataset_type in ["train", "val", "test"]
@@ -37,13 +36,12 @@ class CommaDataset(Dataset):
         self.normalize = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         self.resize = transforms.Resize((224,224))
         #self.resize = transforms.Resize((480,480))
-        data_path = f"{dataset_path}/filtered_chunk1_{dataset_type}.hdf5" if ground_truth == "regular" else f"{dataset_path}/filtered_chunk1_{dataset_type}.hdf5"
+        data_path = f"{self.dataset_path}/filtered_chunk1_{dataset_type}.hdf5" if ground_truth == "regular" else f"{self.dataset_path}/filtered_chunk1_{dataset_type}.hdf5"
         self.people_seqs = []
         self.h5_file = h5py.File(data_path, "r")
-        #corrupt_idx = 62
+        
         self.keys = list(self.h5_file.keys())
-        #if dataset_type == "train":
-        #    self.keys.pop(corrupt_idx)
+       
             
     def __len__(self):
         return len(self.keys)
