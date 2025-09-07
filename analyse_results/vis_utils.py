014a9abd2e9a6c8af8446f9722b84a7b187a706d
Peppe2212 <117827822+Peppe2212@users.noreply.github.com>
2025-09-07 17:44:42 +0200

    Refactor dataset paths in vis_utils.py for consistency and clarity

--- analyse_results/vis_utils.py
+++ analyse_results/vis_utils.py
@@ -8,7 +8,6 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import pad_collate
 from dataloader_comma import CommaDataset
-from dataloader_nuscenes import NUScenesDataset
 from collections import Counter
 from model import VTN
 import matplotlib.pyplot as plt 
@@ -88,19 +87,14 @@ def get_regular_ckpt_from_lightning_checkpoint(state_dict):
 
 commda_ds = CommaDataset(dataset_type="test",
         multitask=multitask,
-        ground_truth="normal", dataset_path='/kaggle/input/filtered-chunk1', return_full=True)
-## modified dataset path to execute on Kaggle By G.V.
+        ground_truth="normal", dataset_path='/kaggle/input/final-hdf5-files', return_full=True)
 
-#nuscenes_ds = NUScenesDataset(dataset_type="test",
-#        multitask=multitask, max_len=20,
-#        ground_truth="normal", dataset_path='/data1/jessica/data/toyota/')
 dataloader_comma = DataLoader(commda_ds, batch_size=1, shuffle=False, num_workers=0)
-#dataloader_nuscenes = DataLoader(nuscenes_ds, batch_size=1, shuffle=True, num_workers=0, collate_fn=pad_collate)
 
 model = VTN(multitask=multitask, backbone=backbone, concept_features=concept_features, device = f"cuda:{gpu_num}", return_concepts=True)
-checkpoint_path_distance = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_comma_distance_none/lightning_logs/version_0/checkpoints//epoch=50-step=3162.ckpt'
-checkpoint_path_angle = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_nuscenes_distance_none/lightning_logs/version_10/checkpoints/epoch=519-step=14560.ckpt'
-p = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_comma_distance_none_True_ablation/lightning_logs/version_2/checkpoints/epoch=77-step=19266.ckpt'
+#checkpoint_path_distance = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_comma_distance_none/lightning_logs/version_0/checkpoints//epoch=50-step=3162.ckpt'
+#checkpoint_path_angle = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_nuscenes_distance_none/lightning_logs/version_10/checkpoints/epoch=519-step=14560.ckpt'
+#p = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_comma_distance_none_True_ablation/lightning_logs/version_2/checkpoints/epoch=77-step=19266.ckpt'
 #p = '/data1/jessica/data/toyota/ckpts_final/ckpts_final_comma_distance_none_True_ablation/lightning_logs/version_2/checkpoints/epoch=64-step=16055.ckpt'
-checkpoint_path = checkpoint_path_distance
+#checkpoint_path = checkpoint_path_distance
 
