2bd68f01c61c9ed93e9a7fb7f8a4c38d436d90ab
Gabriele-Raffaele <gabriele.raffaele87@gmail.com>
2025-09-08 12:09:18 +0200

    Update main directory path for loading logits in VTN class

--- model.py
+++ model.py
@@ -184,7 +184,7 @@ class VTN(nn.Module):
             if self.concept_source == "retinanet":
                 
                 logits_per_image = []
-                main_dir = "/kaggle/input/road-logits"
+                main_dir = "/kaggle/input/noisy-concepts"
                 for key in seq_key:
                     dir = f"{main_dir}/{key}.pt"
                     data = torch.load(dir)
